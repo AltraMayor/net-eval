2ccca594db1d22c63408ddbd0be00cac9391c15f
Michel Machado <michel@digirati.com.br>
2013-03-24 17:29:17 -0400

    Add to-do list of router keeper

---
+++ rk.c
@@ -0,0 +1,34 @@
+/* Router Keeper. */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/* TODO Read:
+	 * options:
+	 *	Prefix file name
+	 *	Stack ("ip" or "xia")
+	 *	Update rate
+	 *	Run (1..)
+	 * arguments:
+	 *	Network interface of gateway (e.g. 'eth0')
+	 *	Gateway address
+	 * Derive:
+	 *	Number of nodes (Packet writers plus router)
+	 */
+
+	/* TODO Load seeds. */
+
+	/* TODO Load and shuffle destinations. */
+
+	/* TODO Convert destinations to a binary vector. */
+
+	/* TODO Load destinations into routing table. */
+	/* TODO Use batch updates! */
+
+	/* TODO Loop: */
+		/* TODO Sample destination and new gateway. */
+		/* TODO Update routing table. */
+
+	return 0;
+}
