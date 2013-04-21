a668782241290f04ad45874ac99f06c421737358
Michel Machado <michel@digirati.com.br>
2013-04-21 08:26:30 -0400

    Add to-do list of packet counter

---
+++ pc.c
@@ -0,0 +1,30 @@
+/* Packet Counter. */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/* TODO Read:
+	 * options:
+	 *	Stack ("ip" or "xia")
+	 *	Flag to add ebtables rules
+	 *	Path where ebtables(8) is located
+	 *	Sleep time between samplings
+	 *	Fully qualified name of the file to save sampling
+	 * arguments:
+	 *	Network interfaces of containers from
+	 *		host's views (e.g. 'veth.test1')
+	 */
+
+	/* TODO Install ebtables rules. */
+
+	/* TODO Create full path for file. */
+
+	/* TODO Create sampling file. */
+
+	/* TODO Loop: */
+		/* TODO Sample measurements and save to file. */
+		/* TODO Sleep. */
+
+	return 0;
+}
